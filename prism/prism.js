9a6ac134c2d52e405002cf6a36d10f1eb173a8fd
huntzhan <programmer.zhx@gmail.com>
2016-11-14 11:30:47 +0800

    Enhanced prism.

--- prism/prism.js
+++ prism/prism.js
@@ -13,6 +13,24 @@ function getScriptWithCache(url, callback) {
 let prism_base = "https://cdn.staticfile.org/prism/1.5.1";
 
 
+function toThemePath(theme) {
+  return `${prism_base}/themes/${theme}.min.css`;
+}
+
+
+function toComponentPath(component) {
+  return `${prism_base}/components/prism-${component}.min.js`;
+}
+
+
+function toPluginPath(plugin) {
+  return [
+    `${prism_base}/plugins/${plugin}/prism-${plugin}.min.css`,
+    `${prism_base}/plugins/${plugin}/prism-${plugin}.min.js`
+  ];
+}
+
+
 function loadCss(path) {  
   let linkText = `<link href='${path}' rel='stylesheet' type='text/css' />`;
   let link = $(linkText);
@@ -20,18 +38,78 @@ function loadCss(path) {
 }
 
 
+function extractDataField(key) {
+  let data = document.currentScript.dataset[key];
+  if (data) {
+    return data.trim().split(/\s+/);
+  } else {
+    return [];
+  }
+}
+
+
+// data-prism-theme="xxx"
+// return css paths.
+function extractTheme() {
+  // prism.min.css, required.
+  let theme = ['prism'];
+  return theme.concat(extractDataField('prism-theme')).map(toThemePath);
+}
+
+
+// data-prism-components="xxx xxx"
+// return js paths.
+function extractComponents() {
+  let components = ['c', 'markup', 'css', 'javascript'];
+  return components.concat(extractDataField('prism-components')).map(toComponentPath);
+}
+
+
+// data-prism-plugins="xxx xxx"
+// return (css, js) paths.
+function extractPlugins() {
+  let pluginCss = [];
+  let pluginJs = [];
+  for (let plugin of extractDataField('prism-plugins')) {
+    let [css, js] = toPluginPath(plugin);
+    pluginCss.push(css);
+    pluginJs.push(js);
+  }
+  return [pluginCss, pluginJs];
+}
+
+
 function loadPrismRest() {  
-  // css
-  loadCss(`${prism_base}/themes/prism.min.css`);
-  // patch style.
-  $('<style> .token.operator { background: hsla(0, 0%, 100%, 0) none repeat scroll 0 0; } </style>').appendTo("head");
+  let prismCss = [];
+  let prismJs = [];
+
+  // theme.
+  prismCss = prismCss.concat(extractTheme());
 
   // components.
-  let components = ['c', 'cpp', 'python', 'ini'];
+  prismJs = prismJs.concat(extractComponents());
+
+  // plugins.
+  let [pluginCss, pluginJs] = extractPlugins();
+  prismCss = prismCss.concat(pluginCss);
+  prismJs = prismJs.concat(pluginJs);
+
+  // load css.
+  for (let cssPath of prismCss) {
+    loadCss(cssPath);
+  }
+  // patch style.
+  $(
+    `<style>
+     .token.operator {
+       background: hsla(0, 0%, 100%, 0) none repeat scroll 0 0;
+     }
+    </style>`
+  ).appendTo("head");
+
+  // load js.
   $.when(
-    ...components.map(
-      component => getScriptWithCache(`${prism_base}/components/prism-${component}.min.js`)
-    )
+    ...prismJs.map(jsPath => getScriptWithCache(jsPath))
   ).done(() => {
     Prism.highlightAll();
   });
